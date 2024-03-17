77df080d400ccb965decd56015ef7905f7a121db
FrankieMNyaga <77462878+FrankieMNyaga@users.noreply.github.com>
2024-03-17 16:23:29 -0400

    Initial commit

---
+++ src/Privacy.java
@@ -0,0 +1,11 @@
+import anonymizer.AnonRecordTable;
+
+public class Privacy {
+    public static void main(String[] args) {
+
+        AnonRecordTable privacy = new AnonRecordTable(new Integer[3000], new Integer[0], "table");
+        System.out.println(privacy);
+        
+
+    }
+}
