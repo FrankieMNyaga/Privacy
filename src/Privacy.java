fcf9e69ea5e5dcd452eb8329b7988537d51a9bb8
FrankieMNyaga <77462878+FrankieMNyaga@users.noreply.github.com>
2024-03-19 13:47:26 -0400

    Anonymization methods update and comments
    Added more degrees of anonymization to anonymize methods, added comments

--- src/Privacy.java
+++ src/Privacy.java
@@ -11,7 +11,9 @@ public class Privacy {
     public Privacy(List<String[]> table) {
         this.table = table;
     }
-
+     /*Returns contents of CSV file as a List<String[]> 
+      * file - String name of input file
+     */
     public static List<String[]> readCSV(String file) {
         List<String[]> values = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
@@ -27,7 +29,10 @@ public class Privacy {
             return null;
         }
     }
-
+    /*Creates output text file of given List<String[]>
+     * values - Input List<String[]> to be written to text file
+     * txtFile - String name of output file 
+     */
     public static void writeTxt(List<String[]> values, String txtFile) {
         try(BufferedWriter bw = new BufferedWriter(new FileWriter(txtFile))) {
             for (String[] row : values) {
@@ -43,6 +48,9 @@ public class Privacy {
         }
     }
     
+    /* Returns String of data at given row
+     * rowIndex - Index of row to be returned
+    */
     public String getRow(int rowIndex) {
         if (rowIndex >= 0 && rowIndex < this.table.size()) {
             StringBuilder builder = new StringBuilder();
@@ -59,7 +67,11 @@ public class Privacy {
             return null;
         }
     }
-
+    
+    /* Returns String of data at given row and column
+     * rowIndex - Index of row where desired cell is found
+     * col - Column index where desired cell is found
+     */
     public String getCell(int rowIndex, int col) {
         String rowStr = this.getRow(rowIndex);
         if(rowStr != null) {
@@ -71,6 +83,11 @@ public class Privacy {
         return null;
     }
 
+    /* Updates String of a given cell
+     * rowIndex - Index of row where desired cell is found
+     * col - Column index where desired cell is found
+     * updateString - String which replaces current cell contents
+     */
     public void setCell(int rowIndex, int col, String updateString) {
         if(rowIndex >= 0 && rowIndex < this.table.size()) {
             String[] row = this.table.get(rowIndex);
@@ -80,6 +97,14 @@ public class Privacy {
         }
     }
 
+    /* Uses '*' to anonymize cells
+     * rowIndex - Index of row to be anonymized
+     * col - Index of column to be anonymized
+     * degree - 0 = every other character is anonymized
+     *          1 = everything between first and last char is anonymized
+     *          2 = everything after first char is anonymized
+     *          3 = everything is anonymized
+     */
     public void anonymizeCell(int rowIndex, int col, int degree) {
         StringBuilder builder = new StringBuilder();
         String cell = this.getCell(rowIndex, col);
@@ -88,9 +113,53 @@ public class Privacy {
                 builder.append("*");
             }
         }
+        if(this.table != null && degree == 2) {
+            for(int i = 0; i < cell.length(); i++) {
+                if(i ==0) {
+                    builder.append(cell.charAt(0));
+                }
+                else {
+                    builder.append("*");
+                }
+            }
+        }
+        if(this.table != null && degree == 1) {
+            for(int i = 0; i < cell.length(); i++) {
+                if(i ==0) {
+                    builder.append(cell.charAt(0));
+                }
+                if(i == cell.length() - 1) {
+                    builder.append(cell.charAt(cell.length() - 1));
+                }
+                else {
+                    builder.append("*");
+                }
+            }
+        }
+        if(this.table != null && degree == 0) {
+            for(int i = 0; i < cell.length(); i++) {
+                if(i == 0) {
+                    builder.append(cell.charAt(0));
+                }
+                if(i % 2 != 0) {
+                    builder.append(cell.charAt(i));
+                }
+                else {
+                    builder.append("*");
+                }
+            }
+        }
         this.setCell(rowIndex, col, builder.toString());
     }
 
+    /* Anonymized every cell in a column after given row
+     * rowStart - Row where anonymization starts
+     * col - Column to be anonymized
+     * degree - 0 = every other character is anonymized
+     *          1 = everything between first and last char is anonymized
+     *          2 = everything after first char is anonymized
+     *          3 = everything is anonymized
+     */
     public void anonymizeCol(int rowStart, int col, int degree) {
         for (int i = 0; i < this.table.size(); i++) {
             for (int j = 0; j < this.table.get(i).length; j++) {
