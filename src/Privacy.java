11e2a2cee130de2f33855d7d37bbfb4cd5afd3e3
FrankieMNyaga <77462878+FrankieMNyaga@users.noreply.github.com>
2024-04-13 18:20:27 -0400

    update

--- src/Privacy.java
+++ src/Privacy.java
@@ -32,12 +32,21 @@ public class Privacy {
     /*Creates output text file of given List<String[]>
      * values - Input List<String[]> to be written to text file
      * txtFile - String name of output file 
+     * startLine - Line to start writing from
+     * endLine - Line to stop writing to txt file
+     * NOTE - always writes the first line (headings line) DO NOT INCLUDE LINE 0
      */
-    public static void writeTxt(List<String[]> values, String txtFile) {
+    public static void writeTxt(List<String[]> values, String txtFile, int startLine, int endLine) {
         try(BufferedWriter bw = new BufferedWriter(new FileWriter(txtFile))) {
-            for (String[] row : values) {
-                for (String cell : row) {
-                    bw.write(cell + " | ");
+            for (int k = 0; k < 1; k++) {
+                for (int l = 0; l < values.get(k).length; l++) {
+                    bw.write(values.get(k)[l] + ", ");
+                }
+            }
+            bw.newLine();
+            for (int i = startLine; i < endLine; i++) {
+                for (int j = 0; j < values.get(i).length; j++) {
+                    bw.write(values.get(i)[j] + ", ");
                 }
                 bw.newLine();
             }
@@ -58,7 +67,7 @@ public class Privacy {
             for (int i = 0; i < row.length; i++) {
                 builder.append(row[i]);
                 if (i < row.length - 1) {
-                    builder.append(" | ");
+                    builder.append(", ");
                 }
             }
             return builder.toString();
@@ -75,7 +84,7 @@ public class Privacy {
     public String getCell(int rowIndex, int col) {
         String rowStr = this.getRow(rowIndex);
         if(rowStr != null) {
-            String[] row = rowStr.split(" | ");
+            String[] row = rowStr.split(", ");
             if (col >= 0 && col < row.length) {
                 return row[col];
             }
@@ -100,14 +109,58 @@ public class Privacy {
     /* Uses '*' to anonymize cells
      * rowIndex - Index of row to be anonymized
      * col - Index of column to be anonymized
-     * degree - 0 = every other character is anonymized
+     * degree - 0 = every other character is anonymized including first and last
      *          1 = everything between first and last char is anonymized
      *          2 = everything after first char is anonymized
      *          3 = everything is anonymized
+     *          4 = year is anonymized, only for DOB (col = 3)
+     *          5 = last two digits of year is anonymized, only for DOB (col = 3)
+     *          6 = last two didgits of year for job_data DOB (col = 15)
+     *          7 = everyother letter including first letter
      */
     public void anonymizeCell(int rowIndex, int col, int degree) {
         StringBuilder builder = new StringBuilder();
         String cell = this.getCell(rowIndex, col);
+        if(this.table != null && degree == 7) {
+            for(int i = 0; i < cell.length(); i++) {
+                if(i % 2 == 0) {
+                    builder.append(cell.charAt(i));
+                }
+                else {
+                    builder.append("*");
+                }
+            }
+        }
+        if(this.table != null && degree == 6) {
+            for(int i = 0; i < cell.length(); i++) {
+                if(i < 2) {
+                    builder.append(cell.charAt(i));
+                }
+                else {
+                    builder.append("*");
+                }
+            }
+        }
+        if(this.table != null && degree == 5) {
+            for(int i = 0; i < cell.length(); i++) {
+                if(i < 8) {
+                    builder.append(cell.charAt(i));
+                }
+                else {
+                    builder.append("*");
+                }
+            }
+        }
+        if(this.table != null && degree == 4) {
+            for(int i = 0; i < cell.length(); i++) {
+                if(i < 6) {
+                    builder.append(cell.charAt(i));
+                }
+                else {
+                    builder.append("*");
+                }
+            }
+        }
         if(this.table != null && degree == 3) {
             for(int i = 0; i < cell.length(); i++) {
                 builder.append("*");
@@ -115,8 +168,8 @@ public class Privacy {
         }
         if(this.table != null && degree == 2) {
             for(int i = 0; i < cell.length(); i++) {
-                if(i ==0) {
-                    builder.append(cell.charAt(0));
+                if(i == 0) {
+                    builder.append(cell.charAt(i));
                 }
                 else {
                     builder.append("*");
@@ -125,11 +178,11 @@ public class Privacy {
         }
         if(this.table != null && degree == 1) {
             for(int i = 0; i < cell.length(); i++) {
-                if(i ==0) {
-                    builder.append(cell.charAt(0));
+                if(i == 0) {
+                    builder.append(cell.charAt(i));
                 }
-                if(i == cell.length() - 1) {
-                    builder.append(cell.charAt(cell.length() - 1));
+                else if(i == cell.length() - 1) {
+                    builder.append(cell.charAt(i));
                 }
                 else {
                     builder.append("*");
@@ -138,9 +191,6 @@ public class Privacy {
         }
         if(this.table != null && degree == 0) {
             for(int i = 0; i < cell.length(); i++) {
-                if(i == 0) {
-                    builder.append(cell.charAt(0));
-                }
                 if(i % 2 != 0) {
                     builder.append(cell.charAt(i));
                 }
@@ -149,6 +199,7 @@ public class Privacy {
                 }
             }
         }
+        
         this.setCell(rowIndex, col, builder.toString());
     }
 
@@ -170,6 +221,16 @@ public class Privacy {
         }
     }
 
+    /* Randomizes the order of rows after the first row (table headings) */
+    public static void shuffle(List<String[]> rows) {
+        for (int i = 1; i < rows.size(); i++) {
+            int randomIndex = (int) (Math.random() * (rows.size() - 1)) + 1;
+            String[] temp = rows.get(i);
+            rows.set(i, rows.get(randomIndex));
+            rows.set(randomIndex, temp);
+        }
+    }
+
     public static void main(String[] args) {
         String job = "resources/job_data.csv";
         String personal = "resources/personal_data.csv";
@@ -179,7 +240,16 @@ public class Privacy {
         List<String[]> personalList = readCSV(personal);
         Privacy jobPrivacy = new Privacy(jobList);
         Privacy personalPrivacy = new Privacy(personalList);
-        writeTxt(jobList, jobTxt);
-        writeTxt(personalList, personalTxt);
+        shuffle(jobList);
+        shuffle(personalList);
+        personalPrivacy.anonymizeCol(1, 1, 2);
+        personalPrivacy.anonymizeCol(1, 3, 5);
+        personalPrivacy.anonymizeCol(1, 5, 2);
+        personalPrivacy.anonymizeCol(1, 4, 2);
+        jobPrivacy.anonymizeCol(1, 15, 6);
+        jobPrivacy.anonymizeCol(1, 14, 2);
+        jobPrivacy.anonymizeCol(1, 10, 2);
+        writeTxt(jobList, jobTxt, 1, 30);
+        writeTxt(personalList, personalTxt, 1, 30);
     }
 }
